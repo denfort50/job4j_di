4979c60d7b369319e4d617992b89593606a422d8
denfort50 <denfort50@yandex.ru>
2023-02-12 17:52:54 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,23 @@
+package ru.job4j.di;
+
+public class StartUI {
+
+    private final Store store;
+    private final ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+    public void add() {
+        store.add(consoleInput.askStr("Введите имя..."));
+
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
